f82a3fc2f274c87982dedc26ee6d27b495861b10
Jess2 <jess2.developer@gmail.com>
2022-04-23 17:23:33 +0900

    페이지 구조 설계 및 라우터 설정 (#12)

---
+++ src/lib/banner/Banner.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import '../../app.css';
+</script>
+
+<div class="banner">
+    <h2>Banner</h2>
+</div>
+
+<style>
+    .banner {
+        height: 400px;
+    }
+</style>
---
+++ src/lib/footer/Footer.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+    import logo from '../../assets/images/itsuda-logo.png';
+    import '../../app.css';
+</script>
+
+<footer>
+    <h2 class="hidden">Footer</h2>
+    <div class="corner">
+        <a href="/">
+            <img src={logo} alt="Itsuda" />
+        </a>
+    </div>
+</footer>
+
+<style>
+    footer {
+        display: flex;
+        justify-content: space-between;
+        height: var(--footer-height);
+        background-color: #3b3b3b;
+    }
+
+    .corner {
+        width: 3em;
+        height: 3em;
+    }
+
+    .corner a {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+        height: 100%;
+    }
+
+    .corner img {
+        width: 2em;
+        height: 2em;
+        object-fit: contain;
+    }
+</style>
